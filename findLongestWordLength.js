aa982a9d9e762151a3889414001304ee2d3988d7
Siege Teodosio <siegeteodosio15@gmail.com>
2019-12-17 02:38:23 +0800

    add 2nd version for findLongestWord

--- findLongestWordLength.js
+++ findLongestWordLength.js
@@ -10,3 +10,12 @@ function findLongestWordLength(str) {
 }
 
 console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
+
+const findLongestWordLengthv2 = str => {
+  return str
+  .split(" ")
+  .reduce((x,y)=>{ return Math.max(x, y.length)},0);  
+}
+
+
+console.log(findLongestWordLengthv2("The quick brown fox jumped over the lazy dog"));
