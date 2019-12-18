050f6a693ad4d18bfac404b1fa2ba54171b49808
Siege Teodosio <siegeteodosio15@gmail.com>
2019-12-18 18:16:22 +0800

    add solution

--- frankenSplice.js
+++ frankenSplice.js
@@ -1,6 +1,7 @@
 function frankenSplice(arr1, arr2, n) {
   // It's alive. It's alive!
+  arr2.splice(n,0,...arr1);
   return arr2;
 }
 
-frankenSplice([1, 2, 3], [4, 5, 6], 1);
+console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
