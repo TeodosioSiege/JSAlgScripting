ab57c361adf84c6dad513b33b3fed7464e1a5d06
Siege Teodosio <siegeteodosio15@gmail.com>
2020-01-27 18:16:49 +0800

    version 2

--- IntermediateAlgs/sumAll.js
+++ IntermediateAlgs/sumAll.js
@@ -1,11 +1,21 @@
 function sumAll(arr) {
   let newArr = arr.sort(function(a,b){return a - b});
-  let sum = newArr[0];
-  for(let i = newArr[0] + 1; i < newArr[newArr.length - 1] + 1;
+  let sum = 0;
+  for(let i = newArr[0]; i <= newArr[newArr.length - 1];
    i++){
-      sum += i;
+    sum += i;
   }
   return sum;
 }
 
-console.log(sumAll([4, 1]));
+//console.log(sumAll([4, 1]));
+const sumAllv2 = arr => {
+  let sum = 0;
+  for(let i = Math.min(...arr); i <= Math.max(...arr); i++){
+    sum += i;
+  }
+  return sum;
+}
+
+sumAllv2([4,1]);
+
