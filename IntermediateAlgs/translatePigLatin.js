a278620002d17cf744f7d70725006209760dbb83
Siege Teodosio <siegeteodosio15@gmail.com>
2020-02-03 21:26:01 +0800

    pig latin

---
+++ IntermediateAlgs/translatePigLatin.js
@@ -0,0 +1,20 @@
+function translatePigLatin(str) {
+  const vowel = ['a','e','i','o','u'];
+  let pig = str;
+  if(vowel.indexOf(pig[0]) > -1) {
+    pig += "way";
+  } else {
+    let cluster = "";
+    let arr = str.split('');
+    let i = 0;
+    while (vowel.indexOf(pig[i]) == -1 && i < str.length) {
+      cluster += pig[i];
+      arr.shift();
+      i++;
+    }
+      pig = arr.join('') + cluster + 'ay';
+  }
+  return pig;
+}
+
+console.log(translatePigLatin("rhythm"));
