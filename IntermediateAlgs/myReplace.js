5467803c547d34423e6032864cc8ce8507a30d88
Siege Teodosio <siegeteodosio15@gmail.com>
2020-02-04 12:01:29 +0800

    replace alg

---
+++ IntermediateAlgs/myReplace.js
@@ -0,0 +1,11 @@
+function myReplace(str, before, after) {
+  let arr = str.split(' ');
+  let regExp = /^[A-Z]/;
+  let capitalize;
+  regExp.test(arr[arr.indexOf(before)]) ? 
+    capitalize = after.charAt(0).toUpperCase() + after.slice(1) : capitalize = after;
+  arr[arr.indexOf(before)] = capitalize;
+  return arr.join(' ');
+}
+
+console.log(myReplace("A quick brown fox Jumped over the lazy dog", "Jumped", "leaped"));
