7db7c6ea7e0563c6df48e18e465e368866b63a69
Siege Teodosio <siegeteodosio15@gmail.com>
2020-02-05 10:22:44 +0800

    pair element

---
+++ IntermediateAlgs/pairElement.js
@@ -0,0 +1,17 @@
+function pairElement(str) {
+  let dna = [];
+  function dnaJoiner(strand){
+    switch(strand){
+      case 'A': return ['A','T'];break;
+      case 'T': return ['T','A']; break;
+      case 'C': return ['C','G'];break;
+      case 'G': return ['G','C'];break;
+    }
+  }
+  for(let i = 0; i < str.length; i++){
+    dna.push(dnaJoiner(str[i]));
+  }
+    return dna;
+}
+
+console.log(pairElement("GCG"));
