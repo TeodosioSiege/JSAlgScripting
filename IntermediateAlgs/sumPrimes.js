44d395e05cd63a33d26934d1055e67fa169f501e
Siege Teodosio <siegeteodosio15@gmail.com>
2020-02-10 18:40:14 +0800

    sumPrimes

---
+++ IntermediateAlgs/sumPrimes.js
@@ -0,0 +1,15 @@
+function sumPrimes(num) {
+  let arr = [];
+  
+
+  for(let i = 1; i <= num; i++){
+      arr.push(i);
+    }
+    for(let divisor = 2;divisor < num; divisor++){
+    arr = arr.filter(integer =>(integer % divisor > 0 || integer == divisor) && integer > 1);}
+    return arr.reduce((a,b)=> a + b);
+    
+}  
+
+
+console.log(sumPrimes(977));
