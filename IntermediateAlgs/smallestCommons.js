a15cbecbd27902ef2db7233631f99a3f69d958bd
Siege Teodosio <siegeteodosio15@gmail.com>
2020-02-11 21:09:23 +0800

    update lcm

--- IntermediateAlgs/smallestCommons.js
+++ IntermediateAlgs/smallestCommons.js
@@ -14,18 +14,45 @@ function smallestCommons(arr) {
     }
     return primeArr;
   }
-  const gcf = (arr1, arr2) => {
-    let returnVal = 1;
-    let mutablearr2 = [...arr2];
-    for(let i = 0; i < arr1.length; i++){if(mutablearr2.includes(arr1[i])){
-      returnVal *= arr1[i];
-      let itemRemove = mutablearr2.indexOf(arr1[0]);
-      let start = mutablearr2.slice(0,itemRemove);
-      let end = mutablearr2.slice(itemRemove + 1);
-      mutablearr2 = start.concat(end);
-    }}
-     return returnVal;
+  const gcfFinder = (num1, num2) => {
+    let factors1 = primeFactors(num1);
+    let factors2 = primeFactors(num2);
+    let bigger;
+    let smaller;
+    let gcf = 1;
+    if(factors1.length >= factors2.length){
+      bigger = factors1;
+      smaller = factors2;
+    } else {
+      bigger = factors2;
+      smaller = factors1;
+    } 
+    for(let i = 0; i < smaller.length; i++){
+      if(bigger.includes(smaller[i])){
+      gcf *= smaller[i];
+      let terminator = bigger.indexOf(smaller[i]);
+      let start = bigger.slice(0,terminator);
+      let end = bigger.slice(terminator + 1);
+      bigger = start.concat(end);
+    }
+    }
+    
+    return gcf
   }
+  gcfFinder(6,12);
+
+
+  let lcm = newArr[newArr.length - 1];
+  let factors1;
+  let factors2;
+  for(let i = newArr.length - 2; i >= 0; i--){
+   let prev = lcm;
+  let gcf = gcfFinder(lcm,newArr[i]);
+   lcm *= (newArr[i])/ gcf;
+  console.log(`${lcm} = (${newArr[i]} * ${prev}) / ${gcf}`);
+  }
+  
+  return lcm;
 }
 
 
