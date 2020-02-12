99e14ee8f5417b79fa51a532c997b206aef6dfdb
Siege Teodosio <siegeteodosio15@gmail.com>
2020-02-12 20:42:36 +0800

    dropElements solution

---
+++ IntermediateAlgs/dropElements.js
@@ -0,0 +1,13 @@
+function dropElements(arr, func) {
+  // Drop them elements.
+  let newArr = [];
+  for(let i = 0; i < arr.length; i++){
+    if(func(arr[i])){
+      newArr = arr.slice(arr.indexOf(arr[i]));
+      break;
+    }
+  }
+  return newArr;
+}
+
+dropElements([1, 2, 3], function(n) {return n < 3; });
