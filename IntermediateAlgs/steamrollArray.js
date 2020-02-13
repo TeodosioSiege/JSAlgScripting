91099d020b14c1134ddc1069ec6b31b88f6ee65e
Siege Teodosio <siegeteodosio15@gmail.com>
2020-02-13 11:39:55 +0800

    steamrollArray solution

---
+++ IntermediateAlgs/steamrollArray.js
@@ -0,0 +1,14 @@
+function steamrollArray(arr) {
+  // I'm a steamroller, baby
+  let newArr = [];
+  for(let i = 0; i < arr.length; i++){
+    if (arr[i].length == undefined || typeof arr[i] == 'string'){
+      newArr.push(arr[i]);
+    } else {
+      newArr = newArr.concat(steamrollArray(arr[i]));
+    }
+  }
+  return newArr;
+}
+
+steamrollArray([[["a"]], [["b"]]]);
